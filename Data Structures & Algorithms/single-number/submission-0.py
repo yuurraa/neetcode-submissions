28cd8df4a89fcce89f00e8720901e51ad2b87d68
yuurraa <joshua.chin@live.com>
2026-07-30 13:05:37 +0800

    feat: added keycloak

--- Data Structures & Algorithms/single-number/submission-0.py
+++ Data Structures & Algorithms/single-number/submission-0.py
@@ -1,4 +1,6 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        counter = Counter(nums)
-        return min(counter, key=counter.get)
+        res = 0
+        for n in nums:
+            res = res ^ n
+        return res
